af207ddea1f74aa11fbbd9e5fd109743b2485d22
Robert Meyer <rmeyer93@fpuna.edu.py>
2023-09-05 20:06:51 -0400

    Create UsuarioRequest.cs

---
+++ CapaEntidad/Models/UsuarioRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad.Models
+{
+    public class UsuarioRequest
+    {
+        public string Login { get; set; }
+
+        public string Password { get; set; }    
+
+        public string UsuarioSucursalCaja { get; set; }
+
+        public int EmpleadoId { get; set; }
+
+        public string UsuarioRol { get; set; }
+
+        public string UsuarioSucursal { get; set; }
+
+        public bool Activo { get; set; }
+    }
+}
