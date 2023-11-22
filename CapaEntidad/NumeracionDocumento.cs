aa79fb41c46b86b3626d35dffd79e90d86799dd8
Robert Meyer <rmeyer93@fpuna.edu.py>
2023-11-21 23:18:14 -0300

    NumeracionDocumento

---
+++ CapaEntidad/NumeracionDocumento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class NumeracionDocumento
+    {
+        public int IdTimbrado { get; set; }
+
+        public int IdCaja { get; set; }
+
+        public string CodigoEstablecimiento { get; set; }
+
+        public int PuntoEmision { get; set; }
+
+        public int UltimoNumero { get; set; }
+    }
+}
